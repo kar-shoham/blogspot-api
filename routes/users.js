0e74ff0a13aacb3cebdf357c5a1c5605d58ae35b
Shoham <kar.shoham.25@gmail.com>
2023-07-01 17:11:41 +0530

    initial commit

---
+++ routes/users.js
@@ -0,0 +1,52 @@
+const router = require("express").Router();
+const Post = require("../models/Post");
+const User = require("../models/User");
+
+//Update
+router.put("/:id",async (req,res)=>{
+    if(req.body.userId === req.params.id){
+        try{
+       const updatedUser = await User.findByIdAndUpdate(
+        req.params.id,{
+            $set: req.body,
+        },{new:true} ) ;
+        res.status(200).json(updatedUser);
+        } catch (err) {
+            res.status(500).json({message:"Something went wrong"});
+        }
+    }else{
+        res.status(401).json("You can update only your account!");
+    }
+});
+//DELETE
+router.delete("/:id", async (req, res) => {
+    if (req.body.userId === req.params.id) {
+      try {
+        const user = await User.findById(req.params.id);
+        try {
+          await Post.deleteMany({ username: user.username });
+          await User.findByIdAndDelete(req.params.id);
+          res.status(200).json("User has been deleted...");
+        } catch (err) {
+          res.status(500).json({message:"Something went wrong"});
+        }
+      } catch (err) {
+        res.status(404).json("User not found!");
+      }
+    } else {
+      res.status(401).json("You can delete only your account!");
+    }
+  });
+
+//GET USER
+router.get("/:id", async (req,res) =>{
+    try{
+        const user = await User.findById(req.params.id);
+        res.status(200).json(user);
+    }catch (err) {
+     res.status(500).json({message:"Something went wrong"});
+    }
+}
+)
+
+module.exports = router;
